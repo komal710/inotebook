560d3587ce7ba53f6affbe4673e1865b94b19d97
Komal Singh <komalsingh7102000@gmail.com>
2024-04-20 22:12:44 +0530

    connected MongoDB

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI ="mongodb://localhost:27017/"
+const mongoURI ="mongodb://localhost:27017/?directConnection=true&readPreference=primary&tls=false&authMechanism=DEFAULT"
 
 const connectToMongo = async () => {
     try {
